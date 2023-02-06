0bffb6fd8ff8dff73d4b326acc7312ecefa2d17d
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-02-06 10:48:28 +0200

    adding Buy function

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
 
+export class ProductsComponent {
+  fortune: number =9800
+ constructor( ){}
+prod: Product = {
+  name: "Burger",
+  price: 45,
+  
+}
+
+buy(){
+  this.fortune = this.fortune - this.prod.price
+}
 }
